ee02779563600c3b50ea5b8db971662ce56a8223
Administrator <admin@example.com>
2022-12-02 11:42:38 +0900

    환율 수정

--- api/cost_inform.py
+++ api/cost_inform.py
@@ -1,11 +1,11 @@
 import boto3
 import botocore
-import yfinance as yf
+import urllib.request as req
+import re
 from datetime import datetime, timedelta, date
 from dateutil.relativedelta import relativedelta
+from bs4 import BeautifulSoup
 
-
-#pip install yfinance 필수!
 
 #요금 조회
 def getCost(access_key_set,secret_key_set,region):
@@ -13,8 +13,9 @@ def getCost(access_key_set,secret_key_set,region):
         access_key = access_key_s['aws_access_key_id']
         secret_key = secret_key_s['aws_secret_access_key']
 
+        sum_cos = 0
 
-        now = datetime.now()
+
         '''
         past = []
         past.append(str(now.year))
@@ -29,14 +30,15 @@ def getCost(access_key_set,secret_key_set,region):
 
         yester_day = str(date.today() - timedelta(1))
 
-
         list_cos = []
         list_date = []
 
-
-        data = yf.download(['USDKRW=X'], start=past_day, end=yester_day)
-
-        change = data['Close'][-1]
+        # 환율 받기
+        url = "https://finance.naver.com/marketindex/"
+        res = req.urlopen(url)
+        soup = BeautifulSoup(res, "html.parser")
+        rate = str(soup.select_one("div.head_info > span.value").string)
+        f_rate = re.sub(",", "", rate)
 
         client = boto3.client(
             'ce',
@@ -54,7 +56,7 @@ def getCost(access_key_set,secret_key_set,region):
                 Metrics=['BlendedCost'],
                 Filter={
                     'Tags': {
-                        'Key': 'EKS_cluster',
+                        'Key': 'eks',
                         'Values': [
                             '',
                             ],
@@ -62,16 +64,86 @@ def getCost(access_key_set,secret_key_set,region):
                     }
                 )
 
-            for i in range(1, now.day):
-                float_won = response['ResultsByTime'][i - 1]['Total']['BlendedCost']['Amount']
-                won = round(float(float_won) * change, 2)
+            for i in response['ResultsByTime']:
+                float_won = i['Total']['BlendedCost']['Amount']
+                won = round(float(float_won) * float(f_rate), 2)
                 list_cos.append(int(won))
-                list_date.append(response['ResultsByTime'][i - 1]['TimePeriod']['Start'])
-            print(list_date)
-            result = {'dates' : list_date, 'costs' : list_cos}
+                list_date.append(i['TimePeriod']['Start'])
+
+            for i in range(0,len(list_cos)):
+                sum_cos = sum_cos + list_cos[i]
+
+            print(sum_cos)
+
+            result = {'dates' : list_date, 'costs' : list_cos, 'sum_cos' : sum_cos }
 
             return result
         except botocore.exceptions.ClientError as err:
 
             result = { 'dates' : ['1001-01-01'], 'costs' : ['0','0','0'] }
+            return result
+
+
+
+def ec2Cost(access_key_set,secret_key_set,region):
+    for access_key_s, secret_key_s in zip(access_key_set, secret_key_set):
+        access_key = access_key_s['aws_access_key_id']
+        secret_key = secret_key_s['aws_secret_access_key']
+
+        ec2_sum = 0
+
+
+        past_day = str(date.today() - relativedelta(months=1))
+
+        to_day = str(date.today() - timedelta(hours=1))
+
+        yester_day = str(date.today() - timedelta(1))
+
+        lists_cos = []
+
+        # 환율 받기
+        url = "https://finance.naver.com/marketindex/"
+        res = req.urlopen(url)
+        soup = BeautifulSoup(res, "html.parser")
+        rate = str(soup.select_one("div.head_info > span.value").string)
+        f_rate = re.sub(",", "", rate)
+
+        client = boto3.client(
+            'ce',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name=region
+        )
+        try:
+            response = client.get_cost_and_usage(
+                TimePeriod={
+                'Start': past_day,                # yyyy-mm-ddThh:mm:ss
+                'End': to_day
+                },
+                Granularity='DAILY',                   #'DAILY'|'MONTHLY'|'HOURLY'
+                Metrics=['BlendedCost'],
+                Filter={
+                    'Tags': {
+                        'Key': 'ec2',
+                        'Values': [
+                            '',
+                            ],
+                        }
+                    }
+                )
+
+            for i in response['ResultsByTime']:
+                float_won = i['Total']['BlendedCost']['Amount']
+                won = round(float(float_won) * float(f_rate), 2)
+                lists_cos.append(int(won))
+
+            for i in range(0,len(lists_cos)):
+
+                ec2_sum = ec2_sum + lists_cos[i]
+            result = {'ec2_costs' : lists_cos, 'ec2_sum' : ec2_sum}
+
+            return result
+        except botocore.exceptions.ClientError as err:
+
+            result = { 'ec2_costs' : ['0','0','0'] }
             return result
